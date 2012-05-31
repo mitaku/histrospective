cf799f68dbca70fa987805cdd997420e81f64d8a
Takumi MIURA <mitaku1104@gmail.com>
2012-06-01 00:29:41 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Histrospective::Application.config.secret_token = 'dcadde753ce8cb5447d4a735585e0540bb685b0baacaed711e75850aec8215c52188591684f5727b68e9c4d2eaa48371446569522200cde89e397b0c8f817854'
